47edb49ec794e5e70abd25b47e7f55a355bf9467
SeungBin <natty0337@gmail.com>
2022-12-04 22:53:15 +0900

    feat: 이미지 캐로셀

--- prepare/front/components/PostImages.js
+++ prepare/front/components/PostImages.js
@@ -1,16 +1,89 @@
-import PropTypes from 'prop-types';
-import React from 'react';
-const PostImages =  ({images}) => {
+import React, { useCallback, useState } from "react";
+import PropTypes from "prop-types";
+import { PlusOutlined } from "@ant-design/icons";
+import ImagesZoom from "./ImagesZoom";
 
-    return(
+
+const PostImages = ({ images }) => {
+  const [showImagesZoom, setShowImagesZoom] = useState(false);
+
+  const onZoom = useCallback(() => {
+    setShowImagesZoom(true);
+  }, []);
+
+  const onClose = useCallback(() => {
+    setShowImagesZoom(false);
+  }, []);
+
+  if (images.length === 1) {
+    return (
+      <>
+        <img
+          role="presentation"
+          src={images[0].src}
+          alt={images[0].src}
+          onClick={onZoom}
+        />
+        {showImagesZoom && <ImagesZoom images={images} onClose={onClose} />}
+      </>
+    );
+  }
+  if (images.length === 2) {
+    return (
+      <>
         <div>
-        구현중
+          <img
+            role="presentation"
+            src={images[0].src}
+            alt={images[0].src}
+            width="50%"
+            onClick={onZoom}
+          />
+          <img
+            role="presentation"
+            src={images[1].src}
+            alt={images[1].src}
+            width="50%"
+            onClick={onZoom}
+          />
         </div>
-    )
-}
+        {showImagesZoom && <ImagesZoom images={images} onClose={onClose} />}
+      </>
+    );
+  }
+  return (
+    <>
+      <div>
+        <img
+          role="presentation"
+          src={images[0].src}
+          alt={images[0].src}
+          width="50%"
+          onClick={onZoom}
+        />
+        <div
+          role="presentation"
+          style={{
+            display: "inline-block",
+            width: "50%",
+            textAlign: "center",
+            verticalAlign: "middle",
+          }}
+          onClick={onZoom}
+        >
+          <PlusOutlined />
+          <br />
+          {images.length - 1}
+          개의 사진 더보기
+        </div>
+      </div>
+      {showImagesZoom && <ImagesZoom images={images} onClose={onClose} />}
+    </>
+  );
+};
 
-PostImages.prototype ={
-    images: PropTypes.arrayOf(PropTypes.any)
-}
+PostImages.prototype = {
+  images: PropTypes.arrayOf(PropTypes.any),
+};
 
 export default PostImages;
